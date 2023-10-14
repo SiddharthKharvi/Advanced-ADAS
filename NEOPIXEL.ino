cd5eeb128fa1f83eb77ab66e39e4837b41992c5b
Siddharth Kharvi <50961569+SiddharthKharvi@users.noreply.github.com>
2023-10-14 23:03:59 +0530

    Update NEOPIXEL.ino

--- NEOPIXEL.ino
+++ NEOPIXEL.ino
@@ -1,28 +1,31 @@
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
-#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
-#define NUMPIXELS 16 // Popular NeoPixel ring size
+#include <Adafruit_NeoPixel.h>\
+#define PIN        6 // On Trinket or Gemma, sugge
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(3, 6, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
 
 void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 }
 
 void loop() {
+  //RED
   pixels.clear(); // Set all pixel colors to 'off'
-  pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-
+  pixels.setPixelColor(0, pixels.Color(225, 0, 0));
   pixels.show();   // Send the updated pixel colors to the hardware.
-
+  delay(3000);
+  //YELLo
+  pixels.clear(); // Set all pixel colors to 'off'
+  pixels.setPixelColor(1, pixels.Color(255, 255, 0));
+  pixels.show();   // Send the updated pixel colors to the hardware.
+  delay(500);
+  //GREEN
+  pixels.clear(); // Set all pixel colors to 'off'
+  pixels.setPixelColor(2, pixels.Color(00, 255, 0));
+  pixels.show();   // Send the updated pixel colors to the hardware.
+  delay(2000);
   delay(DELAYVAL); // Pause before next pass through loop
-}
+
 }
