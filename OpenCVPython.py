70ec447853fe5e5aec46415d64f6142aee32591e
Siddharth Kharvi <50961569+SiddharthKharvi@users.noreply.github.com>
2023-10-15 06:22:51 +0530

    Update OpenCVPython.py

--- OpenCVPython.py
+++ OpenCVPython.py
@@ -5,8 +5,9 @@ import time
 import serial
   
 cap = cv2.VideoCapture(0)
-ser = serial.Serial('COM8', 9600, timeout=0.050)
 
+ser = serial.Serial('COM8', 9600, timeout=0.050)
+SM = 'I'
 
 while(cap.isOpened()): 
       
@@ -38,8 +39,32 @@ while(cap.isOpened()):
             print("S")
             ser.write(bytes('S', 'utf-8'))
 
+        if keyboard.is_pressed("0"):
+            if SM == 'I':
+                SM = 'A'
+                print("Traffic Signal Light Recon Active..!")
+                time.sleep(2)
+
+                
+            else:
+                SM = 'I'
+                print("Traffic Signal Light Recon Inactive..!")
+                time.sleep(2)
+
+        if SM == 'A':
+            b, g, r = cv2.split(img)
+            total_red_intensity = r.sum()
+            total_green_intensity = g.sum()
+
+            if total_red_intensity > total_green_intensity:
+                print ("Red Signal")
+                time.sleep(1)80
+                ser.write(bytes('S', 'utf-8'))
+                
+            else:
+                print ("Green Signal")
+
         
-            
     cap.release() 
     cv2.destroyAllWindows() 
 else: 
